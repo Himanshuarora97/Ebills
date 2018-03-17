62110d5d85e300fada4f9d539439308f55ba9e30
anmolduainter <anmolduainter@gmail.com>
2018-03-17 11:57:07 +0530

    Added Cache

---
+++ app/src/main/java/com/ebills/alphamind/ebills/Storage/CacheStorage/CacheStorage.java
@@ -0,0 +1,57 @@
+package com.ebills.alphamind.ebills.Storage.CacheStorage;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+import com.ebills.alphamind.ebills.R;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.lang.reflect.Type;
+import java.util.Stack;
+
+/**
+ * Created by anmol on 16/3/18.
+ */
+
+public class CacheStorage {
+
+    Context ctx;
+
+    public CacheStorage(Context ctx){
+        this.ctx = ctx;
+    }
+
+    public void saveFile(String path) throws JSONException {
+
+        String pth = getFile();
+        Type listType =
+                new TypeToken<Stack<String>>(){}.getType();
+        Stack<String> stack = new Gson().fromJson(pth, listType);
+
+        if (stack.size()>5){
+            stack.pop();
+        }
+        else{
+            stack.push(path);
+        }
+
+        String json = new Gson().toJson(stack);
+
+        SharedPreferences sharedPref = ctx.getSharedPreferences(String.valueOf(R.string.Cache_Storage_EBILLS), Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPref.edit();
+        editor.putString(String.valueOf(R.string.Cache_Storage_EBILLS), json);
+        System.out.println("-----------------------------------------> Success saved");
+        editor.commit();
+    }
+
+    public String getFile(){
+        SharedPreferences sharedPref = ctx.getSharedPreferences(String.valueOf((R.string.Cache_Storage_EBILLS)), Context.MODE_PRIVATE);
+        return sharedPref.getString(String.valueOf(R.string.Cache_Storage_EBILLS), "[]");
+    }
+
+}
