c69c7ddfa7e6e442bb78a7a8239aad015a37cf91
himanshuarora97 <iamhimanshuarora97@gmail.com>
2018-03-19 20:11:07 +0530

    Get notifications

--- app/src/main/java/com/ebills/alphamind/ebills/Notifications/FirebaseMessage.java
+++ app/src/main/java/com/ebills/alphamind/ebills/Notifications/FirebaseMessage.java
@@ -5,11 +5,15 @@ import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.support.v4.app.NotificationCompat;
+import android.util.Log;
 
 import com.ebills.alphamind.ebills.MainActivity;
 import com.ebills.alphamind.ebills.R;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 /**
  * Created by source on 3/15/18.
@@ -24,6 +28,18 @@ public class FirebaseMessage extends FirebaseMessagingService {
         NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this);
         notificationBuilder.setContentTitle("FCM notification");
         notificationBuilder.setContentText(remoteMessage.getNotification().getBody());
+
+        // Getting data
+        JSONObject jsonObject = null;
+        String pdf,html;
+        try {
+            jsonObject = new JSONObject(remoteMessage.getData().get("json"));
+            pdf = remoteMessage.getData().get("pdf");
+            html = remoteMessage.getData().get("html");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        Log.e("onMessageReceived: ", remoteMessage.getData().toString());
         notificationBuilder.setAutoCancel(true);
         notificationBuilder.setSmallIcon(R.mipmap.ic_launcher);
         notificationBuilder.setContentIntent(pendingIntent);
